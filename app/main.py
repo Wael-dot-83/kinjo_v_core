5ec4b6dd98deafdfd8709f2722b12d7d6e4a8bc5
Wael Alzyadat <se@zuj.edu.jo>
2026-07-18 01:14:56 +0300

    chore: remove stale artifacts from commit

--- app/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from app.routers.admin_reports import router as admin_reports_router
-
-app = FastAPI(title="KinJo Admin Reports")
-app.include_router(admin_reports_router)
